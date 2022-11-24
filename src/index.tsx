64b02db6579aa55c4b7d9ae938d4aeaf80950f60
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2022-11-24 01:05:47 -0600

    Connected react with apollo for graphql

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const container = document.getElementById('root');
+/* GraphQL Imports */
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/graphql", // will need to make it an environmet variable
+  cache: new InMemoryCache(),
+});
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab2.css";
+
+import { useQuery, gql } from "@apollo/client";
+import { useEffect } from "react";
 
 const Tab2: React.FC = () => {
+  const { loading, error, data } = useQuery(gql`
+    query GetTranslations {
+      getTranslations {
+        _id
+        name
+        abbreviation
+        language
+        lang
+        books {
+          bookName
+          bibleId
+        }
+      }
+    }
+  `);
+
+  useEffect(() => {
+    console.log(loading);
+    console.log(data);
+  });
+
   return (
     <IonPage>
       <IonHeader>
