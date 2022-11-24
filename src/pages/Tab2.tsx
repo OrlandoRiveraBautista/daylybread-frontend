259436e9b5769a1a82fe7f2f25c19d377988cbcc
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2022-11-24 15:15:13 -0600

    Set up some styles and added google product font

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,6 @@
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
   IonPage,
@@ -35,17 +37,21 @@ const Tab2: React.FC = () => {
 
   return (
     <IonPage>
+      {/* Header for Android */}
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>
+            {loading ? "loading..." : data.getTranslations[0].name}
+          </IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              {loading ? "loading..." : data.getTranslations[0].abbreviation}
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
+      {/* Header for iOS */}
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab3.css";
 
 const Tab3: React.FC = () => {
   return (
@@ -11,11 +17,6 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
