d8936724f0d395468b12fecd50637fcf6541f1c9
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2023-06-18 22:47:47 -0500

    Added verse context and actions

--- src/context/context.ts
+++ src/context/context.ts
@@ -6,12 +6,16 @@ import {
   ITranslation,
   IBookInterface,
   IChapterInterface,
+  IVerseInterface,
 } from "../interfaces/BibleInterfaces";
 
 const context = constate(() => {
   const [chosenTranslation, setChosenTranslation] = useState<ITranslation>();
   const [chosenBook, setChosenBook] = useState<IBookInterface>();
   const [chosenChapter, setChosenChapter] = useState<IChapterInterface>();
+  const [selectedVerseList, setSelectedVerseList] = useState<IVerseInterface[]>(
+    []
+  );
 
   const setTranslation = (dto: ITranslation) => {
     setChosenTranslation(dto);
@@ -25,13 +29,37 @@ const context = constate(() => {
     setChosenChapter(dto);
   };
 
+  const addVerseToList = (dto: IVerseInterface) => {
+    //grab the selected verse list into temp
+    var temp = selectedVerseList;
+
+    // add to the temp
+    temp?.push(dto);
+
+    //set to state
+    setSelectedVerseList(temp);
+  };
+
+  const removeVerseFromList = (dto: IVerseInterface) => {
+    //grab the selected verse list into temp
+    var temp = selectedVerseList?.filter((obj) => {
+      return obj.verse !== dto.verse;
+    });
+
+    //set to state
+    setSelectedVerseList(temp);
+  };
+
   return {
     chosenTranslation,
     chosenBook,
     chosenChapter,
+    selectedVerseList,
     setTranslation,
     setBook,
     setChapter,
+    addVerseToList,
+    removeVerseFromList,
   };
 });
 
