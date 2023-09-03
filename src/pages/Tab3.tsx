ee6da8e25fb564e7179c8c7f1d26492168597fb7
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2023-09-02 19:14:45 -0500

    Added a condition to only render the profile if the userInfo context is present

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,16 +1,25 @@
 import { IonContent, IonPage } from "@ionic/react";
 
 /* Components */
-import Auth from "../components/Auth/Auth";
 
 /* Styles */
 import "./Tab3.scss";
 
+/* Context */
+import { useAppContext } from "../context/context";
+import { Redirect } from "react-router";
+
 const Tab3: React.FC = () => {
+  const { userInfo } = useAppContext();
+
   return (
     <IonPage>
       <IonContent>
-        <Auth />
+        {!userInfo ? (
+          <Redirect to="/login" />
+        ) : (
+          <div>Hello welcome mate to your profile {userInfo.firstName}</div>
+        )}
       </IonContent>
     </IonPage>
   );
