7f5e14e4a65c4583650f22d555135070cc985f96
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2023-09-17 16:51:42 -0500

    Update gql and created createBookmark route hook

--- src/hooks/UserHooks.tsx
+++ src/hooks/UserHooks.tsx
@@ -1,5 +1,5 @@
 import { gql } from "../__generated__/gql";
-import { useLazyQuery } from "@apollo/client";
+import { useLazyQuery, useMutation } from "@apollo/client";
 
 /* Queries */
 const Me = gql(`
@@ -20,12 +20,57 @@ const Me = gql(`
     }
 `);
 
+const createBookmark = gql(`
+  mutation Mutation($options: BookmarkOptions!) {
+    createBookmark(options: $options) {
+      errors {
+        field
+        message
+      }
+      results {
+        _id
+        createdAt
+        updatedAt
+        author {
+          _id
+          firstName
+          lastName
+        }
+        note
+        verses {
+          _id
+          translation {
+            name
+            abbreviation
+          }
+          bookName
+          chapterNumber
+          verse
+          text
+          bibleId
+        }
+      }
+    }
+  }
+`);
+
 /* When using lazy  */
 export const useMe = () => {
   const [getMe, { loading, error, data }] = useLazyQuery(Me);
 
   return {
     getMe,
+    loading,
+    error,
+    data,
+  };
+};
+
+export const useCreateBookmarks = () => {
+  const [setBookmarks, { loading, error, data }] = useMutation(createBookmark);
+
+  return {
+    setBookmarks,
     loading,
     error,
     data,
