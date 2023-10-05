afefed6fe561b84c5e28044bc6ffe773243113f2
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2023-10-05 08:09:59 -0500

    Added the user update to the user api hook; and added bookmarks api hooks

--- src/hooks/UserHooks.tsx
+++ src/hooks/UserHooks.tsx
@@ -13,13 +13,46 @@ const Me = gql(`
               firstName
               lastName
               churchName
+              bioText
               dob
               count
             }
         }
     }
 `);
+const getBookmarks = gql(`
+  query getBookmarks {
+    getMyBookmarks {
+      errors {
+        field
+        message
+      }
+      results {
+        _id
+        createdAt
+        updatedAt
+        author {
+          _id
+        }
+        note
+        verses {
+          _id
+          bookName
+          chapterNumber
+          verse
+          text
+          bibleId
+          translation {
+            abbreviation
+            name
+          }
+        }
+      }
+    }
+  }
+`);
 
+/* Mutations */
 const createBookmark = gql(`
   mutation Mutation($options: BookmarkOptions!) {
     createBookmark(options: $options) {
@@ -54,9 +87,32 @@ const createBookmark = gql(`
   }
 `);
 
-const getBookmarks = gql(`
-  query getBookmarks {
-    getMyBookmarks {
+const userUpdate = gql(`
+  mutation UpdateUser($options: UserUpdateInput!) {
+    updateUser(options: $options) {
+      user {
+        _id
+        createdAt
+        updatedAt
+        email
+        firstName
+        lastName
+        churchName
+        bioText
+        dob
+        count
+      }
+      errors {
+        field
+        message
+      }
+    }
+  }
+`);
+
+const updateBookmark = gql(`
+  mutation UpdateBookmark($updateBookmarkId: String!, $options: BookmarkOptions!) {
+    updateBookmark(id: $updateBookmarkId, options: $options) {
       errors {
         field
         message
@@ -67,21 +123,19 @@ const getBookmarks = gql(`
         updatedAt
         author {
           _id
-          firstName
-          lastName
         }
         note
         verses {
           _id
+          translation {
+            name
+            abbreviation
+          }
           bookName
           chapterNumber
           verse
           text
           bibleId
-          translation {
-            abbreviation
-            name
-          }
         }
       }
     }
@@ -100,6 +154,29 @@ export const useMe = () => {
   };
 };
 
+export const useUserUpdate = () => {
+  const [setUserUpdate, { loading, error, data }] = useMutation(userUpdate);
+
+  return {
+    setUserUpdate,
+    loading,
+    error,
+    data,
+  };
+};
+
+export const useUpdateBookmark = () => {
+  const [setBookmarkUpdate, { loading, error, data }] =
+    useMutation(updateBookmark);
+
+  return {
+    setBookmarkUpdate,
+    loading,
+    error,
+    data,
+  };
+};
+
 export const useCreateBookmarks = () => {
   const [setBookmarks, { loading, error, data }] = useMutation(createBookmark);
 
@@ -111,6 +188,21 @@ export const useCreateBookmarks = () => {
   };
 };
 
+//hook for upon mount
 export const useGetBookmarks = () => {
   return useQuery(getBookmarks);
+};
+
+export const useLazyGetBookmarks = () => {
+  const [getLazyBookmarks, { loading, error, data }] = useLazyQuery(
+    getBookmarks,
+    { fetchPolicy: "cache-and-network" }
+  );
+
+  return {
+    getLazyBookmarks,
+    loading,
+    error,
+    data,
+  };
 };
