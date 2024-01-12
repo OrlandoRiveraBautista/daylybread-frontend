786b4a2f6921d4a062511f4243fa9164fc2534bc
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-01-11 23:50:03 -0600

    Added a randomly gen UUID as a device id in local state session

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,11 @@ const App: React.FC = () => {
    * This function will only be called on the first time a user has come to the site
    */
   const setSession = () => {
-    localStorage.set("session", { session: true, firstTime: true }); // set the local storage session
+    localStorage.set("session", {
+      session: true,
+      firstTime: true,
+      deviceid: crypto.randomUUID(),
+    }); // set the local storage session
     setHasSession(true); // set hasSession to true
   };
 
@@ -118,9 +122,17 @@ const App: React.FC = () => {
   const getSession = async () => {
     // look for a session
     const val = await localStorage.get("session");
+    // check if there is a session
     if (!val) {
       setHasSession(false);
       setFirstTimeFlag(true);
+    }
+    /**
+     * ! This will be deleted later, for now we need to check if the deviceId has been set so that the ai works properly
+     */
+    if (!val.deviceid) {
+      val.deviceid = crypto.randomUUID();
+      localStorage.set("session", val);
     }
     setHasSession(val.session); // set hasSession depending on the response
     setFirstTimeFlag(val.firstTime);
