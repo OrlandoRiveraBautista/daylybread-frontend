372629cad926e6d19549c2ef7eb13c43ec84e0bc
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-05-27 01:16:39 -0500

    Created a hook to set the latest bible history to the url

---
+++ src/components/utility/hooks/useSetBibleHistory.ts
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useHistory, useParams } from "react-router";
+import { useUserBibleHistory } from "../../../hooks/UserHooks";
+import { BibleReadParams } from "../../../assets/ts/types";
+
+const useSetBibleHistory = () => {
+  const urlParams = useParams<BibleReadParams>();
+  const history = useHistory();
+  const { data: userBibleHistoryData } = useUserBibleHistory();
+
+  useEffect(() => {
+    // check for needed values
+    if (!userBibleHistoryData) return;
+    if (Object.keys(urlParams).length) return;
+
+    // get the current user bible history
+    const currentUserBibleHistory =
+      userBibleHistoryData?.me?.user?.bibleHistory?.find(
+        (history) => history.current
+      );
+
+    // get it's latest history
+    const latestHistory = currentUserBibleHistory?.history[0];
+
+    // Get the current URL
+    const currentUrl = history.location.pathname;
+
+    history.push(
+      `${currentUrl}/${latestHistory?.language}/${latestHistory?.bibleAbbr}/${latestHistory?.bookId}/${latestHistory?.chapterNumber}`
+    );
+  }, [userBibleHistoryData]);
+
+  return { userBibleHistoryData };
+};
+
+export default useSetBibleHistory;
