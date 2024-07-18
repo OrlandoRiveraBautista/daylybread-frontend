548d73881e18d17ddc02a68bf4ab7a0f2218bb23
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-07-17 22:45:36 -0500

    Put all the logic of rerouting of the google login into the google auth component

--- src/pages/Auth/GoogleAuth.tsx
+++ src/pages/Auth/GoogleAuth.tsx
@@ -1,22 +1,44 @@
 import React, { useEffect } from "react";
+import { useHistory } from "react-router";
 import { GoogleLogin } from "@react-oauth/google";
 
 /* Graphql Hooks */
 import { useLoginWithGoogle } from "../../hooks/AuthHooks";
 import { LoginWithGoogleMutationMutation } from "../../__generated__/graphql";
 
+/* State */
+import { useAppContext } from "../../context/context";
+
 interface IGoogleAuth {
-  onSuccess: (data: LoginWithGoogleMutationMutation) => void;
+  onSuccess?: (data: LoginWithGoogleMutationMutation) => void;
   onError?: () => void;
 }
 
 const GoogleAuth: React.FC<IGoogleAuth> = ({ onSuccess }: IGoogleAuth) => {
+  const history = useHistory();
+
+  const { setUser } = useAppContext();
+
   const { loginWithGoogle, data } = useLoginWithGoogle();
 
   useEffect(() => {
-    if (!data?.loginWithGoogle.user?._id) return;
+    if (!data?.loginWithGoogle) return;
 
-    onSuccess(data);
+    const { user, errors } = data.loginWithGoogle;
+    if (!user?._id) return;
+    if (errors?.length) return;
+
+    setUser(user);
+
+    if (!user.dob) {
+      history.push("/signupupdateuser");
+    } else {
+      history.push("/me");
+    }
+
+    if (onSuccess) {
+      onSuccess(data);
+    }
   }, [data]);
 
   return (
