d24ca9f8b7d032486d55dd0b4c801a16c999905c
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-10-02 01:41:32 -0500

    Updated the navigation buttons

--- src/components/BibleViewer/NavigationButtons/NavigationButtons.tsx
+++ src/components/BibleViewer/NavigationButtons/NavigationButtons.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { IonFab, IonFabButton, IonIcon } from "@ionic/react";
 import { chevronBack, chevronForward } from "ionicons/icons";
+
+/* Components */
+import BreadCrumbsModal from "../../BreadCrumbsModal/BreadCrumbsModal";
+
+/* Context */
+import { useAppContext } from "../../../context/context";
 
 /* Styles */
 import "./NavigationButtons.scss";
@@ -12,12 +18,29 @@ import useBibleNavigator from "../../utility/hooks/useBibleNavigator";
 import BreadCrumbsIcon from "../../../assets/icons/BreadCrumbs-icon.svg";
 
 const NavigationButtons: React.FC = () => {
+  /* Context */
+  const { resetVersesInList } = useAppContext();
+
   const { nextChapter, backChapter } = useBibleNavigator();
+
+  //   const [selectedElement, setSelectedElement] = useState<Array<string>>([]);
+  const [openSelectedVersesModal, setOpenSelectedVersesModal] =
+    useState<boolean>(false);
+  const [initialModalBreakpoint, setInitialModalBreakpoint] =
+    useState<number>(0.25);
+
+  const handleOpenVerseModal = () => {
+    if (!openSelectedVersesModal) resetVersesInList();
+
+    setOpenSelectedVersesModal(!openSelectedVersesModal);
+    setInitialModalBreakpoint(0.75);
+  };
+
   return (
     <>
       <IonFab id="navigation-buttons">
         {/* Back button */}
-        <IonFabButton color="light" size="small" className="right">
+        <IonFabButton color="light" size="small" className="left">
           <IonIcon icon={chevronBack} onClick={() => backChapter()} />
         </IonFabButton>
 
@@ -25,7 +48,8 @@ const NavigationButtons: React.FC = () => {
         <IonFabButton
           color="light"
           size="small"
-          // onClick={handleOpenVerseModal}
+          className="bread-crumbs-button"
+          onClick={handleOpenVerseModal}
         >
           <IonIcon
             class="bread-crumbs-icon"
@@ -35,10 +59,17 @@ const NavigationButtons: React.FC = () => {
         </IonFabButton>
 
         {/* Forward button */}
-        <IonFabButton color="light" size="small" className="right">
+        <IonFabButton color="primary" size="small" className="right">
           <IonIcon icon={chevronForward} onClick={nextChapter} />
         </IonFabButton>
       </IonFab>
+      {/* bible assistant modal */}
+      <BreadCrumbsModal
+        isOpen={openSelectedVersesModal}
+        onDismiss={handleOpenVerseModal}
+        // selectedText={selectedElement}
+        initialBreakpoint={initialModalBreakpoint}
+      />
     </>
   );
 };
