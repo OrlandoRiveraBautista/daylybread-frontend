1af18ce6154f8cf85ee781a0dfcaa1e70e006817
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-10-07 01:14:18 -0500

    Wrapped the text viewer in a forward Ref to be able to access the breadcrumbs modal

--- src/components/BibleViewer/TextViewer/TextViewer.tsx
+++ src/components/BibleViewer/TextViewer/TextViewer.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 import { IonCol } from "@ionic/react";
 
 /* Components */
@@ -18,6 +23,9 @@ interface ITextViewer {
   verses: BbVerse[];
   isLoading: boolean;
 }
+type TextViewerRefType = {
+  handleOpenVerseModal: () => void;
+};
 
 /**
  * Function to render loading skeleton animation
@@ -39,156 +47,162 @@ const renderSkeleton = () => {
   return items;
 };
 
-const TextViewer: React.FC<ITextViewer> = ({
-  verses,
-  isLoading,
-}: ITextViewer) => {
-  /* Context */
-  const {
-    chosenChapterNumber,
-    chosenChapterVerses,
-    chosenBook,
-    chosenBible,
-    currentMediaTimeStamp,
-    selectedVerseList,
-    addVerseToList,
-    removeVerseFromList,
-    resetVersesInList,
-  } = useAppContext();
+const TextViewer = forwardRef<TextViewerRefType, ITextViewer>(
+  ({ verses, isLoading }: ITextViewer, ref) => {
+    /* Context */
+    const {
+      chosenChapterNumber,
+      chosenChapterVerses,
+      chosenBook,
+      chosenBible,
+      currentMediaTimeStamp,
+      selectedVerseList,
+      addVerseToList,
+      removeVerseFromList,
+      resetVersesInList,
+    } = useAppContext();
 
-  const [selectedElement, setSelectedElement] = useState<Array<string>>([]);
-  const [openSelectedVersesModal, setOpenSelectedVersesModal] =
-    useState<boolean>(false);
-  const [initialModalBreakpoint, setInitialModalBreakpoint] =
-    useState<number>(0.25);
+    const [selectedElement, setSelectedElement] = useState<Array<string>>([]);
+    const [openSelectedVersesModal, setOpenSelectedVersesModal] =
+      useState<boolean>(false);
+    const [initialModalBreakpoint, setInitialModalBreakpoint] =
+      useState<number>(0.25);
 
-  const { mediaTimestamps } = useBibleMedia();
+    const { mediaTimestamps } = useBibleMedia();
 
-  useEffect(() => {
-    const chapterViewerWrapper = document.getElementById("chapter-viewer");
+    useEffect(() => {
+      const chapterViewerWrapper = document.getElementById("chapter-viewer");
 
-    if (chapterViewerWrapper?.style) {
-      if (!openSelectedVersesModal) {
-        setSelectedElement([]);
-        resetVersesInList();
-        chapterViewerWrapper.style.gap = "0px";
-      } else {
-        chapterViewerWrapper.style.gap = "120px";
+      if (chapterViewerWrapper?.style) {
+        if (!openSelectedVersesModal) {
+          setSelectedElement([]);
+          resetVersesInList();
+          chapterViewerWrapper.style.gap = "0px";
+        } else {
+          chapterViewerWrapper.style.gap = "120px";
+        }
       }
-    }
-  }, [openSelectedVersesModal]); // eslint-disable-line react-hooks/exhaustive-deps
+    }, [openSelectedVersesModal]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleMouseDown = (event: string) => {
-    // get the desired html element
-    const span = document.getElementById(event);
-    // get the verse numbers
-    const verseNumber = span?.innerText.split(":")[0];
+    const handleMouseDown = (event: string) => {
+      // get the desired html element
+      const span = document.getElementById(event);
+      // get the verse numbers
+      const verseNumber = span?.innerText.split(":")[0];
 
-    // if no text exit function
-    if (!verseNumber) return;
+      // if no text exit function
+      if (!verseNumber) return;
 
-    const verseObj = chosenChapterVerses?.current![Number(verseNumber) - 1];
+      const verseObj = chosenChapterVerses?.current![Number(verseNumber) - 1];
 
-    if (!verseObj) return;
-    setInitialModalBreakpoint(0.25);
+      if (!verseObj) return;
+      setInitialModalBreakpoint(0.25);
 
-    // check if the state is empty
-    if (selectedElement.length === 0) {
-      addVerseToList(verseObj);
-      setSelectedElement([verseNumber]);
+      // check if the state is empty
+      if (selectedElement.length === 0) {
+        addVerseToList(verseObj);
+        setSelectedElement([verseNumber]);
+        setOpenSelectedVersesModal(true);
+        return;
+      }
+
+      var tempValue = [...selectedElement];
+      // check if the value selected is in the list
+      if (selectedElement.includes(verseNumber)) {
+        const valueIndex = selectedElement.indexOf(verseNumber);
+        if (valueIndex > -1) {
+          tempValue.splice(valueIndex, 1);
+          removeVerseFromList(verseObj);
+          setSelectedElement(tempValue);
+        }
+        if (tempValue.length === 0) {
+          setOpenSelectedVersesModal(false);
+        }
+
+        return;
+      }
+
+      tempValue.push(verseNumber);
+
+      setSelectedElement(tempValue);
       setOpenSelectedVersesModal(true);
+
+      addVerseToList(verseObj!);
       return;
-    }
+    };
 
-    var tempValue = [...selectedElement];
-    // check if the value selected is in the list
-    if (selectedElement.includes(verseNumber)) {
-      const valueIndex = selectedElement.indexOf(verseNumber);
-      if (valueIndex > -1) {
-        tempValue.splice(valueIndex, 1);
-        removeVerseFromList(verseObj);
-        setSelectedElement(tempValue);
-      }
-      if (tempValue.length === 0) {
-        setOpenSelectedVersesModal(false);
-      }
+    const handleOpenVerseModal = () => {
+      if (!openSelectedVersesModal) resetVersesInList();
 
-      return;
-    }
+      setOpenSelectedVersesModal(!openSelectedVersesModal);
+      setInitialModalBreakpoint(0.75);
+    };
 
-    tempValue.push(verseNumber);
+    // Pass the ref to the useImperativeHandle hook
+    useImperativeHandle(ref, () => ({
+      handleOpenVerseModal: () => {
+        handleOpenVerseModal();
+      },
+    }));
 
-    setSelectedElement(tempValue);
-    setOpenSelectedVersesModal(true);
-
-    addVerseToList(verseObj!);
-    return;
-  };
-
-  const handleOpenVerseModal = () => {
-    if (!openSelectedVersesModal) resetVersesInList();
-
-    setOpenSelectedVersesModal(!openSelectedVersesModal);
-    setInitialModalBreakpoint(0.75);
-  };
-
-  return (
-    <>
-      <div className="text-viewer">
-        <>
-          <strong className="chapter-number">{verses[0].chapter}</strong>
-          {isLoading ? (
-            renderSkeleton()
-          ) : (
-            <>
-              {verses.map((verse, index) => (
-                <span
-                  onClick={() =>
-                    handleMouseDown(
+    return (
+      <>
+        <div className="text-viewer">
+          <>
+            <strong className="chapter-number">{verses[0].chapter}</strong>
+            {isLoading ? (
+              renderSkeleton()
+            ) : (
+              <>
+                {verses.map((verse, index) => (
+                  <span
+                    onClick={() =>
+                      handleMouseDown(
+                        chosenBible?.abbr! +
+                          chosenBook?.bookId! +
+                          chosenChapterNumber +
+                          verse.verseStart?.toString()
+                      )
+                    }
+                    id={
                       chosenBible?.abbr! +
-                        chosenBook?.bookId! +
-                        chosenChapterNumber +
-                        verse.verseStart?.toString()
-                    )
-                  }
-                  id={
-                    chosenBible?.abbr! +
-                    chosenBook?.bookId! +
-                    chosenChapterNumber +
-                    verse.verseStart?.toString()
-                  }
-                  key={verse.verseStart?.toString()}
-                  className={`${
-                    selectedVerseList.some(
-                      (sv) => sv.verseStart === verse.verseStart
-                    )
-                      ? "verse-selected"
-                      : ""
-                  } 
+                      chosenBook?.bookId! +
+                      chosenChapterNumber +
+                      verse.verseStart?.toString()
+                    }
+                    key={verse.verseStart?.toString()}
+                    className={`${
+                      selectedVerseList.some(
+                        (sv) => sv.verseStart === verse.verseStart
+                      )
+                        ? "verse-selected"
+                        : ""
+                    } 
                     ${getVerseClass(
                       chosenChapterVerses?.current[index]!,
                       currentMediaTimeStamp,
                       mediaTimestamps!
                     )}
                     `}
-                >
-                  <b>{verse.verseStart}:</b> {verse.verseText}
-                </span>
-              ))}
-            </>
-          )}
-        </>
-      </div>
+                  >
+                    <b>{verse.verseStart}:</b> {verse.verseText}
+                  </span>
+                ))}
+              </>
+            )}
+          </>
+        </div>
 
-      {/* bible assistant modal */}
-      <BreadCrumbsModal
-        isOpen={openSelectedVersesModal}
-        onDismiss={handleOpenVerseModal}
-        selectedText={selectedElement}
-        initialBreakpoint={initialModalBreakpoint}
-      />
-    </>
-  );
-};
+        {/* bible assistant modal */}
+        <BreadCrumbsModal
+          isOpen={openSelectedVersesModal}
+          onDismiss={handleOpenVerseModal}
+          selectedText={selectedElement}
+          initialBreakpoint={initialModalBreakpoint}
+        />
+      </>
+    );
+  }
+);
 
 export default TextViewer;
