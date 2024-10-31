4696ef1620018fd01ba9cf47be5ee94bf0859bfd
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-10-31 00:17:11 -0500

    Spread the data correctly to view the current chapter at a time and batch the verse into 3s

--- src/pages/Storybook/StorybookPage.tsx
+++ src/pages/Storybook/StorybookPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { EffectCards } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -10,8 +10,73 @@ import {
   IonCardSubtitle,
   IonCardTitle,
 } from "@ionic/react";
+import { useAppContext } from "../../context/context";
+import { BbVerse } from "../../__generated__/graphql";
 
 const StorybookPage: React.FC = () => {
+  /* Context */
+  const {
+    chosenChapterVerses,
+    chosenBook,
+    handleResetChapterData,
+    localChapters,
+    isProgrammaticSlide,
+    setIsProgrammaticSlide,
+  } = useAppContext();
+
+  const [batchedVerses, setBatchedVerses] = useState<BbVerse[][]>();
+
+  function batchArray(arr: BbVerse[], size = 3) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
+  }
+
+  useEffect(() => {
+    if (!localChapters?.length) return;
+    if (!chosenChapterVerses?.current) return;
+
+    const currentChapter = localChapters.find(
+      (chap) => chap[0].chapter === chosenChapterVerses.current[0].chapter
+    );
+
+    if (!currentChapter?.length) return;
+
+    const arrayOfBatch = batchArray(currentChapter);
+    setBatchedVerses(arrayOfBatch);
+  }, [chosenChapterVerses]);
+
+  const renderSlides = () => {
+    if (!batchedVerses?.length) return;
+
+    return batchedVerses.map((versesInBatch, index) => {
+      return (
+        <SwiperSlide key={index}>
+          <IonCard>
+            <img
+              alt="Silhouette of mountains"
+              src="https://ionicframework.com/docs/img/demos/card-media.png"
+            />
+
+            <IonCardHeader>
+              <IonCardTitle>
+                Verse:
+                {`${versesInBatch[0].verseStart} -
+                ${versesInBatch[versesInBatch?.length - 1].verseStart}`}
+              </IonCardTitle>
+              <IonCardSubtitle>
+                Chapter: {versesInBatch[0].chapter}
+              </IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent></IonCardContent>
+          </IonCard>
+        </SwiperSlide>
+      );
+    });
+  };
+
   return (
     <div id="storybook-container">
       <Swiper
@@ -23,30 +88,7 @@ const StorybookPage: React.FC = () => {
         slidesPerView={1}
         spaceBetween={30}
       >
-        <SwiperSlide>
-          <IonCard>
-            <img
-              alt="Silhouette of mountains"
-              src="https://ionicframework.com/docs/img/demos/card-media.png"
-            />
-
-            <IonCardHeader>
-              <IonCardTitle>Verse: 1-3</IonCardTitle>
-              <IonCardSubtitle>Chapter: 1</IonCardSubtitle>
-            </IonCardHeader>
-            <IonCardContent>kjfaskldjflaksdj</IonCardContent>
-          </IonCard>
-        </SwiperSlide>
-        <SwiperSlide>
-          <IonCard>
-            <IonCardHeader>Hello</IonCardHeader>
-          </IonCard>
-        </SwiperSlide>
-        <SwiperSlide>
-          <IonCard>
-            <IonCardHeader>Hello</IonCardHeader>
-          </IonCard>
-        </SwiperSlide>
+        {batchedVerses?.length ? renderSlides() : null}
       </Swiper>
     </div>
   );
