4a2005424e3cbe954a594a8a816a80043b2c0073
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-11-18 23:54:39 -0600

    Added the tour target to the init bible picker and added a call to next when the button is pressed

--- src/components/InitialBiblePicker/InitialBiblePicker.tsx
+++ src/components/InitialBiblePicker/InitialBiblePicker.tsx
@@ -9,6 +9,7 @@ import PatternImage from "../../assets/images/Patterns - 4x4.png";
 
 /* Context */
 import { useAppContext } from "../../context/context";
+import { useTour } from "../../context/TourContext";
 
 /* Utils */
 import { displayBibleAbbr } from "../../utils/support";
@@ -22,8 +23,13 @@ const InitialBiblePicker: React.FC = () => {
   const [openSelectedTranslationModal, setOpenSelectedTranslationModal] =
     useState<boolean>(false);
 
-  const handleOpenTranslationModal = () =>
+  const { nextStep } = useTour();
+
+  const handleOpenTranslationModal = () => {
     setOpenSelectedTranslationModal(!openSelectedTranslationModal);
+
+    setTimeout(nextStep, 100);
+  };
 
   return (
     <>
@@ -40,7 +46,7 @@ const InitialBiblePicker: React.FC = () => {
           color="dark"
           size="large"
           onClick={handleOpenTranslationModal}
-          className="translation-button"
+          className="translation-button tour-step-1"
         >
           {chosenBible?.abbr
             ? displayBibleAbbr(chosenBible?.abbr)
