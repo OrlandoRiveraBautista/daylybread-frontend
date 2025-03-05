2dd0bb3c4a79172505f9863e042da6bbba288727
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2025-03-04 23:31:07 -0600

    Trying to get the text to prompt (#94) (#97)
    * Trying to get the text to prompt
    
    * Mergin app tsx
    
    * Updates to the prompting
    
    * Added some screenshots and fixed the manifest to work with PWA
    
    * Updated the return clause to keep the type of the value
    
    * Implemented the prompting with a toast
    
    * Changed the text of the toast and changes the color
    
    * Updated description

---
+++ src/utils/addToHomeScreen.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+// Declare the BeforeInstallPromptEvent interface
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+
+// Extend global WindowEventMap to include beforeinstallprompt event
+declare global {
+  interface WindowEventMap {
+    beforeinstallprompt: BeforeInstallPromptEvent;
+  }
+}
+
+// Taken from: https://gist.github.com/rikukissa/cb291a4a82caa670d2e0547c520eae53
+const useAddToHomescreenPrompt = () => {
+  const [prompt, setState] = useState<BeforeInstallPromptEvent | null>(null);
+
+  const promptToInstall = () => {
+    if (prompt) {
+      return prompt.prompt();
+    }
+    return Promise.reject(
+      new Error(
+        'Tried installing before browser sent "beforeinstallprompt" event'
+      )
+    );
+  };
+
+  useEffect(() => {
+    const ready = (e: BeforeInstallPromptEvent) => {
+      e.preventDefault();
+      setState(e);
+    };
+
+    window.addEventListener("beforeinstallprompt", ready);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", ready);
+    };
+  }, []);
+
+  return { prompt, promptToInstall };
+};
+
+export default useAddToHomescreenPrompt;
