56db5ed138571c4b32d83e4352b3d3cbe2fbeb2c
OrlandoRiveraBautista <orlandovaluta@hotmail.com>
2025-08-24 12:05:35 -0500

    Enhanced the splash screen to always show for a bit and then allow for app entry

--- src/pages/splash/SplashScreen.tsx
+++ src/pages/splash/SplashScreen.tsx
@@ -1,11 +1,4 @@
-import { useEffect, useState } from "react";
-import {
-  IonContent,
-  IonImg,
-  IonPage,
-  IonText,
-  CreateAnimation,
-} from "@ionic/react";
+import { IonContent, IonImg, IonPage, IonText } from "@ionic/react";
 
 /* Styles */
 import "./SplashScreen.scss";
@@ -14,30 +7,11 @@ import "./SplashScreen.scss";
 import WhiteLogo from "../../assets/images/daylybread-logo-white.svg";
 
 const Tab1: React.FC = () => {
-  const [playAnimation, setPlayAnimation] = useState<boolean>(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setPlayAnimation(!playAnimation);
-    }, 2000);
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
   return (
     <IonPage>
       <IonContent fullscreen className="splash-container">
         <div className="splash-container">
-          <CreateAnimation
-            play={playAnimation}
-            duration={1500}
-            iterations={Infinity}
-            keyframes={[
-              { offset: 0, transform: "scale(1)" },
-              { offset: 0.5, transform: "scale(.9)" },
-              { offset: 1, transform: "scale(1)" },
-            ]}
-          >
-            <IonImg src={WhiteLogo}></IonImg>
-          </CreateAnimation>
+          <IonImg src={WhiteLogo}></IonImg>
           <IonText className="product-sans">
             <h2>Daylybread ®</h2>
           </IonText>
